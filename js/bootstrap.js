54c4bf5fd38f2e64ca2490c0eab5222549072b4e
Unknown <arin.er@student.ucll.be>
2019-02-13 00:16:53 +0100

    MDB update

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -3,7 +3,7 @@
   * Copyright 2011-2018 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
   */
-(function (global, factory) {
+ (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('popper.js'), require('jquery')) :
   typeof define === 'function' && define.amd ? define(['exports', 'popper.js', 'jquery'], factory) :
   (factory((global.bootstrap = {}),global.Popper,global.jQuery));
